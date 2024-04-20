4c1dddfb0a3efb67b72f26ec7b3a2644fb080e18
CoolElectronics <me@coolelectronics.me>
2024-04-20 16:55:40 -0400

    add discord and github links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,38 +4,36 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { browserslistToTargets } from "lightningcss";
 import browserslist from "browserslist";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://dreamland.js.org",
-	integrations: [],
-	trailingSlash: "never",
-	markdown: {
-		rehypePlugins: [
-			rehypeHeadingIds,
-			[
-				rehypeAutolinkHeadings,
-				{
-					behavior: "wrap",
-					test: ({ tagName }) => tagName !== "h1"
-				}
-			]
-		]
-	},
-	build: {
-		format: "preserve"
-	},
-	server: {
-		host: true
-	},
-	vite: {
-		css: {
-			transformer: "lightningcss",
-			lightningcss: {
-				targets: browserslistToTargets(browserslist(">= 0.25%"))
-			}
-		},
-		build: {
-			cssMinify: "lightningcss"
-		}
-	}
+  site: "https://dreamland.js.org",
+  integrations: [icon()],
+  trailingSlash: "never",
+  markdown: {
+    rehypePlugins: [rehypeHeadingIds, [rehypeAutolinkHeadings, {
+      behavior: "wrap",
+      test: ({
+        tagName
+      }) => tagName !== "h1"
+    }]]
+  },
+  build: {
+    format: "preserve"
+  },
+  server: {
+    host: true
+  },
+  vite: {
+    css: {
+      transformer: "lightningcss",
+      lightningcss: {
+        targets: browserslistToTargets(browserslist(">= 0.25%"))
+      }
+    },
+    build: {
+      cssMinify: "lightningcss"
+    }
+  }
 });
