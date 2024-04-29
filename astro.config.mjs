500de633abb33c7866f3d2d11cc2f82eaa6d25a9
Patrick Martin <me@psm.pm>
2024-04-28 20:22:43 -0400

    prettierrc + sidebar fixes + center homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,42 +5,46 @@ import { browserslistToTargets } from "lightningcss";
 import browserslist from "browserslist";
 
 import icon from "astro-icon";
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dreamland.js.org",
-  integrations: [icon()],
-  trailingSlash: "never",
-  markdown: {
-    rehypePlugins: [rehypeHeadingIds, [rehypeAutolinkHeadings, {
-      behavior: "wrap",
-      test: ({
-        tagName
-      }) => tagName !== "h1"
-    }]],
-    shikiConfig: {
-      theme: "min-dark",
-      wrap: false,
-      langs: []
-    },
-    integrations: [mdx()]
-  },
-  build: {
-    format: "preserve"
-  },
-  server: {
-    host: true
-  },
-  vite: {
-    css: {
-      transformer: "lightningcss",
-      lightningcss: {
-        targets: browserslistToTargets(browserslist(">= 0.25%"))
-      }
-    },
-    build: {
-      cssMinify: "lightningcss"
-    }
-  }
+	site: "https://dreamland.js.org",
+	integrations: [icon()],
+	trailingSlash: "never",
+	markdown: {
+		rehypePlugins: [
+			rehypeHeadingIds,
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: "wrap",
+					test: ({ tagName }) => tagName !== "h1",
+				},
+			],
+		],
+		shikiConfig: {
+			theme: "min-dark",
+			wrap: false,
+			langs: [],
+		},
+		integrations: [mdx()],
+	},
+	build: {
+		format: "preserve",
+	},
+	server: {
+		host: true,
+	},
+	vite: {
+		css: {
+			transformer: "lightningcss",
+			lightningcss: {
+				targets: browserslistToTargets(browserslist(">= 0.25%")),
+			},
+		},
+		build: {
+			cssMinify: "lightningcss",
+		},
+	},
 });
